ed80c9b619fd509da88df41309a44ff0aeed746c
jana6600 <jananikolic6600@gmail.com>
2021-11-10 04:14:09 -0800

    done

---
+++ tailwind.config.js
@@ -0,0 +1,75 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'Roboto' : ['Roboto', 'sans-serif'],
+        'Poppins' : ['Poppins', 'sans-serif']
+      },
+
+      colors: {
+        gray: {
+          100: "#e0e0e0",
+          200:"#ccc",
+          500: "#555",
+          700: "#222222",
+        },
+
+        red: {
+          300: "#e45447",
+        },
+
+        blue : {
+          light: "#f9f9ff",
+          100: "#90acd1"
+        },
+
+        yellow: {
+          500: "#ffc000"
+        },
+
+        padding: {
+          16: '4.5rem'
+        }
+      },
+
+      lineHeight: {
+        'extra-loose': '2.5',
+      },
+
+      letterSpacing: {
+        widest: '0.18em'
+      },
+
+      height: {
+        xl: '35.7rem',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '720px',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+          },
+        }
+      })
+    }
+  ],
+  important: true,
+}
